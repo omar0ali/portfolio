b1ecd11f0f2900118c26f410d10e554eb7b9b89f
Omar Ali <61897650+omar0ali@users.noreply.github.com>
2023-07-15 18:53:03 -0400

    update portfolio design for mobile

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 const app = Vue.createApp({
     data() {
         return {
+            email: "bajunaidomar@outlook.com",
+            phone: "(613)890-9697",
+            home: "Home",
+            work_exp: "Work Experience",
+            projects: "Projects",
+            contactInfo: "Contact Me",
             showExp: false,
             showContact: false,
             showProjects: false,
             showHome: true,
-            btnTitle: "Show Experience",
+            menu: false,
             toggleExp: function () {
+                this.menu = false;
                 this.showExp = true;
                 this.showProjects = false;
                 this.showContact = false;
                 this.showHome = false;
             },
             toggleProject: function () {
+                this.menu = false;
                 this.showExp = false;
                 this.showProjects = true;
                 this.showContact = false;
                 this.showHome = false;
             },
             toggleContact: function () {
+                this.menu = false;
                 this.showExp = false;
                 this.showProjects = false;
                 this.showContact = true;
                 this.showHome = false;
             },
-            toggleHome() {
+            toggleHome: function() {
+                this.menu = false;
                 this.showExp = false;
                 this.showProjects = false;
                 this.showContact = false;
                 this.showHome = true;
+            },
+            toggleMenu: function() {
+                this.menu = true;
+                this.showExp = false;
+                this.showProjects = false;
+                this.showContact = false;
+                this.showHome = false;
             },
             isMobile() {
                 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
